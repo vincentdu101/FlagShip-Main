fee9b136a9ce49c6c79b40c21ea5279d13d8950f
vincentdu <vincentdu101@gmail.com>
2016-08-10 23:23:40 -0700

    centered header and with responsive navigation bar working

--- app/header/header.component.js
+++ app/header/header.component.js
@@ -13,6 +13,7 @@ var router_1 = require("@angular/router");
 var common_1 = require("@angular/common");
 var http_1 = require("@angular/http");
 var session_service_1 = require("../services/configuration/session.service");
+var dropdown_1 = require("ng2-bootstrap/components/dropdown");
 var HeaderComponent = (function () {
     function HeaderComponent(router, http, Session) {
         this.router = router;
@@ -20,7 +21,22 @@ var HeaderComponent = (function () {
         this.Session = Session;
         this.login = { username: "", password: "" };
         this.user = {};
+        this.mobileToggle = false;
+        this.bars = [
+            { name: "Home", url: "/", active: false },
+            { name: "About", url: "/about", active: false },
+            { name: "Portfolio", url: "/projects", dropdown: [], active: false }
+        ];
+        this.hideDropdownClick();
     }
+    HeaderComponent.prototype.hideDropdownClick = function () {
+        var _this = this;
+        $("html").click(function () {
+            for (var i = 0; i < _this.bars.length; i++) {
+                _this.bars[i].active = false;
+            }
+        });
+    };
     HeaderComponent.prototype.goToPage = function (page, config) {
         if (config === void 0) { config = {}; }
         this.router.navigate([page, config]);
@@ -36,12 +52,15 @@ var HeaderComponent = (function () {
             }
         });
     };
+    HeaderComponent.prototype.triggerSlideToggle = function () {
+        $("nav ul").slideToggle();
+    };
     HeaderComponent = __decorate([
         core_1.Component({
             selector: "header",
             templateUrl: "./app/header/header.component.html",
             providers: [http_1.HTTP_PROVIDERS, session_service_1.Session],
-            directives: [common_1.FORM_DIRECTIVES, common_1.NgIf]
+            directives: [common_1.FORM_DIRECTIVES, common_1.NgIf, dropdown_1.DROPDOWN_DIRECTIVES]
         }), 
         __metadata('design:paramtypes', [router_1.Router, http_1.Http, session_service_1.Session])
     ], HeaderComponent);
--- app/header/header.component.ts
+++ app/header/header.component.ts
@@ -3,23 +3,38 @@ import {Router} from "@angular/router";
 import {FORM_DIRECTIVES, NgIf} from "@angular/common";
 import {Http, HTTP_PROVIDERS} from "@angular/http";
 import {Session} from "../services/configuration/session.service";
+import {DROPDOWN_DIRECTIVES} from "ng2-bootstrap/components/dropdown";
 
 
 @Component({
 	selector: "header",
 	templateUrl: "./app/header/header.component.html",
 	providers: [HTTP_PROVIDERS, Session],
-	directives: [FORM_DIRECTIVES, NgIf]
+	directives: [FORM_DIRECTIVES, NgIf, DROPDOWN_DIRECTIVES]
 })
 
 export class HeaderComponent {
 
 	public login = { username: "", password: "" };
 	public user = {};
+	public mobileToggle = false;
+	public bars = [
+		{ name: "Home", url: "/", active: false },
+		{ name: "About", url: "/about", active: false },
+		{ name: "Portfolio", url: "/projects", dropdown: [], active: false }
+	];
 	
 	constructor(private router: Router, private http: Http,
 				private Session: Session) {
+		this.hideDropdownClick();
+	}
 
+	private hideDropdownClick(): void {
+		$("html").click(() => {
+			for (var i = 0; i < this.bars.length; i++) {
+				this.bars[i].active = false;
+			}
+		});
 	}
 
 	public goToPage(page: string, config = {}) {
@@ -36,6 +51,10 @@ export class HeaderComponent {
 				// $(".login-modal").modal({ show: false });
 			}
 		});
+	}
+
+	public triggerSlideToggle(): void {
+		$("nav ul").slideToggle();
 	}
 
 }
