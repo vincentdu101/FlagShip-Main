955e7b243722aa1a34f183f7908414f0f0aad811
vincentdu101 <vincentdu101@gmail.com>
2018-01-13 23:15:44 -0800

    initial setup of service and component architecture

--- flagship-main/src/app/app.module.ts
+++ flagship-main/src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {CategoryService} from "./services/category/category.service";
+import {ProjectService} from "./services/project/project.service";
+import {CarouselService} from "./services/carousel/carousel.service";
+import {SkillsService} from "./services/skills/skills.service";
+import {PortfolioService} from "./services/portfolio/portfolio.service";
+import {Config} from "./services/configuration/config";
+import {Http} from "@angular/http";
 
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule
